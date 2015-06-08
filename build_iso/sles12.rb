ead55219d189242a2431b0fc145d62eff2721c38
Imobach González Sosa <igonzalezsosa@suse.com>
2015-06-08 08:52:32 +0100

    Don't clobber ISO images that are downloaded yet

--- build_iso/sles12.rb
+++ build_iso/sles12.rb
@@ -23,6 +23,8 @@ obs_url = "http://download.suse.de/ibs/Devel:/YaST:/Head/SLE-12-SP1/"
 iso_url = "http://dist.suse.de/install/SLE-12-Server-GM/SLE-12-Server-DVD-x86_64-GM-DVD1.iso"
 
 base_dir = File.dirname(__FILE__)
+iso_dir = File.join(base_dir, "..", "iso")
+iso_path = File.join(iso_dir, File.basename(iso_url))
 version = File.basename(__FILE__, ".rb")
 cache_dir = File.join(base_dir,"cache")
 obs_packages = File.join(base_dir, version+".obs_packages")
@@ -32,11 +34,16 @@ testing_iso = File.join(base_dir, "../kiwi/iso/testing.iso")
 puts "\n**** Cleanup ****"
 system("rm -rf #{cache_dir+'/*'}")
 
+Dir.chdir(iso_dir) do
+  puts "\n**** Downloading source ISO image ****"
+  system "wget --no-clobber #{iso_url}"
+end
+
 puts "\n**** Fetching all required packages ****"
 system "zypper --root #{cache_dir} ar --no-gpgcheck #{obs_url} yast-packages"
 system "xargs -a #{obs_packages} zypper --root #{cache_dir} --pkg-cache-dir=#{cache_dir} download"
 
-Dir.chdir(File.join( cache_dir, "yast-packages")) {
+Dir.chdir(File.join( cache_dir, "yast-packages")) do
   puts "\n**** Taking user defined RPMs ****"
   File.open(local_packages).each do |package|
     package.strip!
@@ -54,19 +61,16 @@ Dir.chdir(File.join( cache_dir, "yast-packages")) {
     end
   end
 
-  puts "\n**** Downloading source ISO image ****"
-  system "wget #{iso_url}"
-
   puts "\n**** Creating DUD with updated packages ****"
   system "find . -name \"*.rpm\"|xargs mkdud -c #{version}.dud -d sle12 -i instsys,repo --prefix=37"
 
   puts "\n**** Creating new ISO image with the updated packages ****"
-  system "sudo mksusecd -c testing.iso --initrd=#{version}.dud #{File.basename(iso_url)}"
+  system "sudo mksusecd -c testing.iso --initrd=#{version}.dud #{iso_path}"
 
   puts "\n**** Copy new ISO image to veewee/vagrant environment ****"
   puts "\n     destination: #{testing_iso}"
   FileUtils.cp("testing.iso", testing_iso)
-}
+end
 
 puts "\n**** Cleanup ****"
 system("rm -rf #{cache_dir+'/*'}")
