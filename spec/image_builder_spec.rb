c6f1b49158fbe079e3edd76dbde1cddc3c975458
Imobach González Sosa <igonzalezsosa@suse.com>
2015-10-23 12:19:49 +0100

    ImageBuilder#cleanup does not remove testing.iso

--- spec/image_builder_spec.rb
+++ spec/image_builder_spec.rb
@@ -133,7 +133,6 @@ RSpec.describe AYTests::ImageBuilder do
     it "removes copied AutoYaST profile and Veewee definition" do
       expect(FileUtils).to receive(:rm).with(builder.autoinst_path, force: true)
       expect(FileUtils).to receive(:rm).with(builder.definition_path, force: true)
-      expect(FileUtils).to receive(:rm).with(builder.obs_iso_dir.join("testing.iso"), force: true)
       expect(FileUtils).to receive(:rm).with(builder.libvirt_definition_path, force: true)
       builder.cleanup
     end
