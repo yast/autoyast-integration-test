498995f38e47a6fd4687959446fd1b5b8cb988e7
Stefan Schubert <schubi@suse.de>
2015-11-11 11:16:48 +0100

    correct path

--- test/restart_services.sh
+++ test/restart_services.sh
@@ -4,4 +4,4 @@ set -e -x
 # checking if dbus and all wickedd services has not been restarted
 # bnc#944349
 
-grep "restarting services" y2log| grep -v 'dbus.service\|wickedd' && echo "AUTOYAST OK"
+grep "restarting services" /var/log/YaST2/y2log| grep -v 'dbus.service\|wickedd' && echo "AUTOYAST OK"
