b04a2b758b3a5ce59a92206b6a5ba62073447e89
Imobach González Sosa <igonzalezsosa@suse.com>
2015-11-26 17:11:23 +0000

    Convert all specs to lists of tests

--- test/sles12_minimal.rb
+++ test/sles12_minimal.rb
@@ -1,24 +1,5 @@
 require_relative "spec_helper"
 
 describe "SLES 12 checks," do
-  it "if user -vagrant- has been created" do
-    run_test_script("user.sh")
-  end
-
-  # bnc #878427
-  it "if root has /root home" do
-    run_test_script("root.sh")
-  end
-
-  it "after installation snapshot has been created" do
-    run_test_script("installation_snapshot.sh")
-  end
-
-  it "YaST2 packages are not installed" do
-    run_test_script("no_yast2_packages.sh")
-  end
-
-  it "second stage was not executed" do
-    run_test_script("no_second_stage.sh")
-  end
+  include_examples "test_scripts", "sles12_minimal"
 end
--- test/upgrade_sles11.rb
+++ test/upgrade_sles11.rb
@@ -1,36 +1,5 @@
 require_relative "spec_helper"
 
 describe "SLES11 - SLES12 upgrade checks," do
-  it "if user -vagrant- has been created" do
-    run_test_script("user.sh")
-  end
-
-  # bnc #878427
-  it "if root has /root home" do
-    run_test_script("root.sh")
-  end
-
-  it "if dns server and network is available" do
-    run_test_script("dns.sh")
-  end
-
-  # bnc #887126
-  it "if no tmpfs device in autoinst.xml" do
-    run_test_script("tmpfs.sh")
-  end
-
-  # bnc #888168
-  it "if registration is not in autoinst.xml" do
-    run_test_script("no_registration.sh")
-  end
-
-  # bnc #935066
-  it "if ntp time syncing has been passed" do
-    run_test_script("ntp_sync.sh")
-  end
-
-  it "if user scripts have been run" do
-    run_test_script("autoupgrade-userscr.sh")
-  end
-
+  include_examples "test_scripts", "upgrade_sles11"
 end
