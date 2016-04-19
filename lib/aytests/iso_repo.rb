20aa8cd9be5384ad3da566be0747602c4464de44
Stefan Schubert <schubi@suse.de>
2016-04-19 15:42:11 +0200

    fixed iso path

--- lib/aytests/iso_repo.rb
+++ lib/aytests/iso_repo.rb
@@ -50,7 +50,8 @@ module AYTests
       end
       if download_to(uri)
         # Returning the first found ISO in this directory
-        Dir.entries(iso_dir).first
+        filename = Dir.entries(iso_dir).delete_if{ |f| File.directory?(f) || !f.end_with?(".iso") }.first
+        File.join( iso_dir, filename)
       else
         false
       end
